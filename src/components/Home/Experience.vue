33ca01d728d291a96d6d026ec382abb9b4fd0315
Chandralaksana22 <chandralaksana225@gmail.com>
2023-08-06 19:57:05 +0800

    Update : Update Experience

---
+++ src/components/Home/Experience.vue
@@ -0,0 +1,143 @@
+<template>
+  <section class="px-4 py-24 mx-auto max-w-7xl">
+    <h2 class="mb-2 text-3xl font-extrabold leading-tight text-gray-900 mb-5">Experience</h2>
+
+    <ol class="relative border-l border-gray-200 dark:border-gray-700">
+      <li class="mb-10 ml-6">
+        <span
+          class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900"
+        >
+          <svg
+            class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+              d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"
+            />
+          </svg>
+        </span>
+        <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+          Mindimedia
+          <span
+            class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3"
+            >Latest</span
+          >
+        </h3>
+        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+          >July, 2023 - Now</time
+        >
+        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+            As a web developer, my role involves both website maintenance and development for clients. For these tasks, I utilize Winter CMS, JavaScript, and Tailwind to create optimal and visually appealing websites.
+        </p>
+      </li>
+      <li class="mb-10 ml-6">
+        <span
+          class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900"
+        >
+          <svg
+            class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+              d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"
+            />
+          </svg>
+        </span>
+        <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+         Vhiweb
+        </h3>
+        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+          >July 2022 - July 2023</time
+        >
+        <p class="text-base font-normal text-gray-500 dark:text-gray-400">
+            I am a frontend developer. Some of the projects I have completed include Munio, Munio's landing page, Santos Jaya Abadi's website, and Drupadi. Additionally, I developed a JavaScript package for Munio, which was published on npm. I work with Bootstrap, Tailwind, Vue.js, and Svelte.js technologies.
+        </p>
+      </li>
+      <li class="ml-6 mb-10">
+        <span
+          class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900"
+        >
+          <svg
+            class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+              d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"
+            />
+          </svg>
+        </span>
+        <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+        PUI PT DLI
+        </h3>
+        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+          >October 2021 - March 2022</time
+        >
+        <p class="text-base font-normal text-gray-500 dark:text-gray-400">
+          As a Research Assistant at PUI PT DLI, I design a homestay booking app using the design thinking approach. I craft delightful UI/UX using Figma as my primary design tool.
+        </p>
+      </li>
+      <li class="ml-6 mb-10">
+        <span
+          class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900"
+        >
+          <svg
+            class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+              d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"
+            />
+          </svg>
+        </span>
+        <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+       Fakultas Sastra UM
+        </h3>
+        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+          >June 2021 - November 2021</time
+        >
+        <p class="text-base font-normal text-gray-500 dark:text-gray-400">
+            I am a full-stack web developer responsible for building a research-based e-learning website. My tasks encompass designing UI/UX, handling frontend and backend development. The website is tailored to meet the specified targets.
+        </p>
+      </li>
+      <li class="ml-6 mb-10">
+        <span
+          class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900"
+        >
+          <svg
+            class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+              d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"
+            />
+          </svg>
+        </span>
+        <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+        Dinas Pendidikan dan Kebudayaan Kota Malang
+        </h3>
+        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+          >June - August 2021</time
+        >
+        <p class="text-base font-normal text-gray-500 dark:text-gray-400">
+          
+I am a web developer specializing in creating website interfaces from scratch. I build websites using Bootstrap framework for the frontend and PHP for the backend.
+        </p>
+      </li>
+    </ol>
+  </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Experience from '../components/Home/Experience.vue';
 import HeroVue from '../components/Home/Hero.vue';
 import Portofolio from '../components/Home/Portofolio.vue'
 </script>
@@ -7,5 +8,6 @@ import Portofolio from '../components/Home/Portofolio.vue'
   <main>
     <HeroVue/>
     <Portofolio/>
+    <Experience/>
   </main>
 </template>
