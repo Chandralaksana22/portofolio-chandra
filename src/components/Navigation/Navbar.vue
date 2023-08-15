80eb66c42211ce303a71c88f8969e8c6cbf7f38e
Trisna Chandra Laksana <chandralaksana225@gmail.com>
2023-08-15 15:30:55 +0800

    Update : Update Career Page

--- src/components/Navigation/Navbar.vue
+++ src/components/Navigation/Navbar.vue
@@ -18,11 +18,8 @@
       <li class="py-3">
         <router-link to="/portofolio" class="block text-white py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white md:p-0 md:dark:hover:text-white dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Portofolio</router-link>
       </li>
-      <li class="py-3">
-        <router-link to="/career" class="block text-white py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white md:p-0 md:dark:hover:text-white dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Career</router-link>
-      </li>
       <li class="py-3 pr-0 md:pr-5">
-        <a href="#" class="block text-white py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white md:p-0 md:dark:hover:text-white dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Skills</a>
+        <router-link to="/career" class="block text-white py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white md:p-0 md:dark:hover:text-white dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Career</router-link>
       </li>
       </ul>
     </div>
